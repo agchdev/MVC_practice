02d12bdb1c2b003c72e07708e4a3fc1326214569
unknown <alex.chinchi02@gmail.com>
2025-01-14 12:11:41 +0100

    cambios

--- class.autor.php
+++ class.autor.php
@@ -10,5 +10,25 @@
             $this->nombre = $n;
             $this->dni = $d;
         }
+
+        public function obtenerAutores(){
+            $consulta = "SELECT * FROM autores";
+            $sentencia = $this->conn->getCon()->prepare($consulta);
+            $sentencia->execute();
+            $sentencia->bind_result($this->nombre, $this->dni);
+
+            $autores = array();
+
+            while($sentencia->fecth()){
+                $autor = array(
+                    "nombre" => $this->nombre,
+                    "dni" => $this->dni
+                );
+
+                $autores[] = $autor;
+            }
+
+            return $autores;
+        }
     }
 ?>
